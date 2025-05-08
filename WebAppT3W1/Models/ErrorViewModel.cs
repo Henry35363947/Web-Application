919c6f142f2efc55769718d0d9cb00369d896382
Henry35363947 <ihezuohenry1@gmail.com>
2025-05-07 20:25:03 -0400

    push
    testpush

---
+++ WebAppT3W1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppT3W1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
